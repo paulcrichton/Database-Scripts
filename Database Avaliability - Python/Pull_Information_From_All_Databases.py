f8084179d5af2a27763daf3419c83e608f007c77
Paul Crichton <78025872+paulcrichton@users.noreply.github.com>
2025-07-09 18:16:23 +0100

    automatic name fetch

--- Database Avaliability - Python/Pull_Information_From_All_Databases.py
+++ Database Avaliability - Python/Pull_Information_From_All_Databases.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3.9
 
 from Fetch_CDB_PDB_Information.Fetch_CDB_PDB_States import gather_information_from_database as GIFD
+from Fetch_CDB_PDB_Information.Fetch_Database_Home_And_SID import create_db_name_home_array as CDNHA
 import numpy as py
 
 def main():
+    
+    cdb_states, pdb_states = get_states()
+
+    print(cdb_states, pdb_states)
+    
+def create_login_details(name):
     user= "c##paul"
     pwd = "paul"
     host = "prodba-db"
     port = 1521
-    database_name = "IFSLCDB"
+    database_name = name
 
+    return user, pwd, host, port, database_name
 
-
-
+def get_states():
+    db_arr, oh_arr = CDNHA()
     
-    cdb_states, pdb_states = GIFD(user, pwd, host, port, database_name)
-
-    print(cdb_states, pdb_states)
+    for db_name in db_arr:
+        user, pwd, host, port, database_name=create_login_details(db_name)
+        cdb_states, pdb_states = GIFD(user, pwd, host, port, database_name)
     
+    return cdb_states, pdb_states
 
 if __name__ == "__main__":
     main()
