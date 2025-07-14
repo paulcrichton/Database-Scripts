a01eef167c536882271915ed9f310e3837b6b4ed
Paul Crichton <78025872+paulcrichton@users.noreply.github.com>
2025-07-14 17:39:29 +0100

    slight changes

--- Project_Root/Monitoring_Tool/Database_Connections/CDB_or_PDB.py
+++ Project_Root/Monitoring_Tool/Database_Connections/CDB_or_PDB.py
@@ -24,6 +24,8 @@ def cdb_or_pdb(user, pwd, host, port, database_name):
 
     connection.close()
 
+    print(cdb_or_pdb_db_value)
+
     if cdb_or_pdb_db_value == "CDB":
         return "CDB"
     else:
