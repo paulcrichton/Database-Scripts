69987b232dc79295c01be4a157df290f5bb56767
Paul Crichton <78025872+paulcrichton@users.noreply.github.com>
2025-07-15 12:32:32 +0100

    testing

--- Project_Root/Monitoring_Tool/Create_Reports.py
+++ Project_Root/Monitoring_Tool/Create_Reports.py
@@ -169,7 +169,7 @@ def create_report_all():
         pdb_names=get_pluggable_names(container_name)
         if "UNKNOWN" not in pdb_names:
             for pdb_name in pdb_names:
-                print(pdb_name)
+                print(pdb_name, "testing this line here")
                 create_database_report(pdb_name)
 
 
