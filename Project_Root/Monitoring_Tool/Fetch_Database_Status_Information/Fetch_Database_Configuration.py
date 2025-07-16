cf89b618b51c6f5134c942632b95a5a36efc5b87
Paul Crichton <78025872+paulcrichton@users.noreply.github.com>
2025-07-16 13:11:38 +0100

    bug fixing

--- Project_Root/Monitoring_Tool/Fetch_Database_Status_Information/Fetch_Database_Configuration.py
+++ Project_Root/Monitoring_Tool/Fetch_Database_Status_Information/Fetch_Database_Configuration.py
@@ -46,7 +46,7 @@ def gather_configuration_information(user, pwd, host, port, database_name):
 
     print(trace_dir)
 
-    trace_dir = pd.DataFrame([{"PARAMETER" : trace_dir[1], "VALUE": trace_dir.index[2]}])
+    trace_dir = pd.DataFrame([{"PARAMETER" : str(trace_dir[1]), "VALUE": str(trace_dir.index[2])}])
 
     database_configuration_information = pd.concat([database_home_base, trace_dir], ignore_index=True)
 
